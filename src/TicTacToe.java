a676b7c4af2684cbf03413f086891daac4b616d0
vanillecoco <vanillecoco.hs@gmail.com>
2021-06-18 10:34:20 -0700

    update

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -16,19 +16,34 @@ public class TicTacToe {
                 { '-', '+', '-', '+', '-' }, { ' ', '|', ' ', '|', ' ' } };
 
         printGameBoard(gameBoard);
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Enter your placement(1-9) : ");
-        int playerPos = scan.nextInt();
 
-        placePiece(gameBoard, playerPos, "player");
+        while (true) {
+            Scanner scan = new Scanner(System.in);
+            System.out.println("Enter your placement(1-9) : ");
+            int playerPos = scan.nextInt();
+            while (playerPositions.contains(playerPos) || cpuPositions.contains(playerPos)) {
+                System.out.println("Position taken, Enter a correct Position");
+                playerPos = scan.nextInt();
+            }
 
-        Random rand = new Random();
-        int cpuPos = rand.nextInt(9) + 1;
-        placePiece(gameBoard, cpuPos, "cpu");
+            placePiece(gameBoard, playerPos, "player");
 
-        printGameBoard(gameBoard);
-        checkWinner();
+            Random rand = new Random();
+            int cpuPos = rand.nextInt(9) + 1;
+            while (playerPositions.contains(cpuPos) || cpuPositions.contains(cpuPos)) {
+                System.out.println("Position taken, Enter a correct Position");
+                cpuPos = rand.nextInt(9) + 1;
+            }
+            placePiece(gameBoard, cpuPos, "cpu");
 
+            printGameBoard(gameBoard);
+            String result = checkWinner();
+            if (result.length() > 0) {
+                System.out.println(result);
+                break;
+            }
+
+        }
     }
 
     private static void printGameBoard(char[][] gameBoard) {
@@ -78,8 +93,10 @@ public class TicTacToe {
         char symbol = 'X';
         if (user.equals("player")) {
             symbol = 'X';
+            playerPositions.add(pos);
         } else if (user.equals("cpu")) {
             symbol = '0';
+            cpuPositions.add(pos);
         }
         switch (pos) {
             case 1:
